0ef623177f2c884e739fc1d6452eeb2bd09e6743
Baron <popo333555@gmail.com>
2020-06-24 22:44:34 +0200

    change to i > 0

--- password_allen.py
+++ password_allen.py
@@ -1,12 +1,14 @@
 password = 'a123456'
-i = 3
-while True:
+i = 3 # 剩餘機會
+while i > 0:
+	i = i - 1
 	pwd = input('Enter password: ')
 	if pwd == password:
 		print('Welcome to log in')
 		break
-	else:
-		i = i - 1
-		print('Wrong password! you have', i, 'chance')
-		if i == 0:
-			break
+	else:		
+		if i > 0:
+			print('Wrong password! you have', i, 'chance')
+		else:
+			print('No more chance')
+		
